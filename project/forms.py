56f011d304bf55721b8f7ba77d37970d44829738
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 13:10:25 +0300

    Music

--- project/forms.py
+++ project/forms.py
@@ -7,14 +7,13 @@ class RegisterForm(Form):
     username = StringField('Username', validators=[DataRequired(), Length(max=40)])
     password = PasswordField('Password', validators=[DataRequired(), Length(max=40)])
     number = StringField('Phone', validators=[DataRequired(), Length(max=40)])
-    booster_seat_id = StringField('booster_seat_id', validators=[DataRequired(), Length(max=40)])
+    booster_seat_id = StringField('Phone', validators=[DataRequired(), Length(max=40)])
 
 class LoginForm(Form):
     username = StringField('Username', validators=[DataRequired(), Length(max=40)])
     password = PasswordField('Password', validators=[DataRequired(), Length(max=40)])
 
-class AddContactForm(Form):
-    name = StringField('Name', validators=[DataRequired(), Length(max=40)])
-    phone = StringField('Phone', validators=[DataRequired(), Length(max=40)])
-    relation = StringField('Relation', validators=[DataRequired(), Length(max=40)])
-
+#PROBLEM
+class AddContactForm():
+    name = StringField('name', validators=[DataRequired(), Length(max=40)])
+    phone = StringField('phone', validators=[DataRequired(), Length(max=40)])
